5f64f484c60a6e1cb39d70b093acbe30ea6c56a3
JMoon <j.ksiezo@gmail.com>
2019-12-16 21:10:15 +0100

    Development changes

--- MoonScraper.py
+++ MoonScraper.py
@@ -9,38 +9,47 @@ Created on Mon Dec 16 18:14:41 2019
 import requests
 from bs4 import BeautifulSoup
 import argparse
+import os
 
 
 URL = 'https://pracuj.pl/praca'
 
-soup = BeautifulSoup(page.content, 'html.parser')
 
-def PreprocessURL(location=None, keywords):
+def PreprocessURL(keywords, location=None):
     global URL
     if len(keywords) > 1:
         keywords = "-x44-".join(keywords)
     else:
         keywords = keywords[0]
-    URL += kwywords + ';kw/'
+    URL += keywords + ';kw/'
     if location:
         URL += location + ';wp/'
     
     return URL
 
-def 
-    
-        
-    
-    
-page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+def SoupActions(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    offers = soup.find_all(name="offer-details__text")
+    
+    return offers
+    
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(prog="MoonScraper", 
                                  description="Specify Location and Keywords to get info")
-parser.add_argument('-l', '--loc', type=str, help='Location of work offer')
-parser.add_argument('-k', '--keyword', action='append' type=str, help='Main keywords')
-parser.add_argument('-sp' ,'--savepath', type=str, help='path to save directory')
-args = parser.parse_args()
+    parser.add_argument('-l', '--loc', type=str, help='Location of work offer')
+    parser.add_argument('-k', '--keywords', action='append', type=str, help='Main keywords')
+    parser.add_argument('-sp' ,'--savepath', type=str, help='path to save directory')
+    args = parser.parse_args()
+    print(args)
+    if args.savepath:
+        save_dir = os.path.dirname(args.save_path)
+        if not os.path.exists(save_dir):
+                print(f"Save directory doesn't exist. Making {save_dir}")
+                os.makedirs(save_dir)
+    print(SoupActions(PreprocessURL(URL)))
+
+    
